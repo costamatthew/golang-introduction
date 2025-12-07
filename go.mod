fa919f10aa051e18725f3a8bd7efae3687aa2265
Mateus Costa <mateusdsilvacosta@outlook.com>
2025-12-07 12:58:01 -0300

    feat: funcao main, modulo, estrutura, encapsulamento

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/costamatthew/golang-introduction
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/costamatthew/golang-introduction/meet"
+)
+
+func main() {
+	meet.SayHello()
+	meet.Say("Hello")
+}
