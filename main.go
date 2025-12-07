dc2ee8781e0dede368b5de365805fd79169fd353
Mateus Costa <mateusdsilvacosta@outlook.com>
2025-12-07 15:06:22 -0300

    Variáveis e Constantes

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import (
 )
 
 func main() {
+	var texto1 string = "Texto01"
+	var texto2 = "Texto02"
+	texto3 := "Texto03"
+
+	const texto4 string = "Texto04"
+	const texto5 = "Texto05"
+
 	meet.SayHello()
 	meet.Say("Hello")
+	meet.Say(texto1)
+	meet.Say(texto2)
+	meet.Say(texto3)
+	meet.Say(texto4)
+	meet.Say(texto5)
 }
